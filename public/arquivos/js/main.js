3faf3b1fab53efe131b5e0e3ea53e86c3111e76d
Thales Mariano <thales.mariano125@gmail.com>
2023-10-03 16:41:58 -0300

    Aprimoramento na lista de usuarios

--- public/arquivos/js/main.js
+++ public/arquivos/js/main.js
@@ -5,26 +5,13 @@ const form = document.querySelector('form')
 const input = document.querySelector('[name=user-msg]')
 const chatContainer = document.querySelector('#chat-container')
 
+const meContainer = document.querySelector('#me-container')
 const usersList = document.querySelector('#users-list')
 
 const user = {
 	id: null,
 	name: prompt('Qual o seu nome?')
 }
-
-socket.on('connect', () => {
-	// chatContainer.innerHTML += `
-	// 	<div class="w-full text-center">
-	// 		<p class="text-zinc-500 text-sm">
-	// 			<span class="text-zinc-600">
-	// 				Você
-	// 			</span>
-	// 			conectou.
-	// 		</p>	
-	// 	</div>
-		
-	// `
-})
 
 socket.on('start-config', socketId => {
 	user.id = socketId
@@ -48,14 +35,22 @@ socket.on('start-config', socketId => {
 socket.on('users-online', users => {
 	usersList.innerHTML = ' '
 
-	users.forEach(user => {
-		const userName = user.name ? user.name : user.id
-		usersList.innerHTML += `
-			<li class="text-zinc-300 px-2 py-1">
-				<span class="text-zinc-400">User:</span>
-				${userName}
-			</li>
-		`
+	users.forEach(u => {
+		if(user.id !== u.id){
+			const userName = u.name ? u.name : u.id
+			usersList.innerHTML += `
+				<li class="text-zinc-300 px-2 py-1">
+					<span class="text-zinc-400">User:</span>
+					${userName}
+				</li>
+			`
+		}else{
+			meContainer.innerHTML = `
+				<p class="text-zinc-300 px-2 py-1 bg-neutral-950">
+					<span class="text-zinc-400">Você:</span>
+					${u.name ? u.name : u.id}
+				</p>`
+		}
 	})
 })
 
@@ -102,9 +97,10 @@ form.addEventListener('submit', event => {
 })
 
 socket.on('chat-msg', msg => {
+	const userName = msg.user.name ? msg.user.name : 'Anônimo'
 	chatContainer.innerHTML += `
 		<div class="max-w-sm min-w-10" data-user="strange" data-msg="msg">
-			<p class="text-zinc-500"><small class="text-zinc-600">${msg.time}</small> - ${msg.user.name}</p>
+			<p class="text-zinc-500"><small class="text-zinc-600">${msg.time}</small> - ${userName}</p>
 			<div class="p-1 my-1 rounded bg-red-800">
 				<p class="text-white">${msg.message}</p>
 			</div>
