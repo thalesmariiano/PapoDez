941bb3d3dcf5d9bd2c7499bda4841e9758b0dd90
Thales Mariano <thales.mariano125@gmail.com>
2024-02-05 22:08:48 -0300

    user model updated

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,9 +1,9 @@
-
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
 	name: {
 		type: String
+		required: true
 	},
 	username: {
 		type: String,
@@ -11,11 +11,13 @@ const userSchema = new mongoose.Schema({
 	},
 	email: {
 		type: String,
+		required: true,
 		lowercase: true,
 		unique: true
 	},
 	password: {
 		type: String,
+		required: true
 	}
 })
 
